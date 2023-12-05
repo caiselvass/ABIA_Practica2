95b6a9b7f26491f07319a9e6adf1a84777c69d80
caiselvass <caiselvassala@gmail.com>
2023-12-05 17:15:22 +0100

    experiments improved

---
+++ generar_plots_experiments.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+while True:
+    try:
+        experiment = input('Quin experiment vols representar? [1, 2, 3]:')
+        if experiment not in ['1', '2', '3']:
+            raise ValueError
+    except ValueError:
+        print("Error: Introdueix només un dels següents valors: 1, 2, 3.")
+        continue
+    break
+
+if experiment == '1':
+    df = pd.read_csv('./experiments/data/experiment1.csv')
+
+    while True:
+        try:
+            extensio: str = str(input('Quina extensió vols representar? [B, 1, 2, 3]:'))
+            extensio = 'B' if extensio == 'b' else extensio
+            if experiment not in ['B', '1', '2', '3']:
+                raise ValueError
+        except ValueError:
+            print("Error: Introdueix només un dels següents valors: B, 1, 2, 3.")
+            continue
+        break
+        
+    df = df[df['Extensió'] == extensio]
+
+    # Convertir les comes a punts per a poder ser llegides com a números
+    df['Temps'] = df['Temps'].str.replace(',', '.').astype(float)
+
+    # Crear el gràfic
+    plt.figure(figsize=(10, 6))
+    plt.plot(df['N llibres'], df['Temps'], marker='o')  
+    plt.xlabel('N Llibres')
+    plt.ylabel('Temps')
+    plt.title('Temps en funció del nombre de llibres')
+    plt.grid(True)
+    plt.savefig(f'./experiments/plots/experiment1_{extensio}.png')
+    plt.show()
+    print(f'experiment1_{extensio}.png saved')
+
+elif experiment == '2':
+    data = pd.read_csv('./experiments/data/experiment2.csv')
+    # Setting the aesthetic style of the plots
+    sns.set(style="whitegrid")
+
+    # Creating subplots for different variables
+    fig, axes = plt.subplots(2, 2, figsize=(15, 10))
+
+    # Plotting the relationships
+    sns.lineplot(ax=axes[0, 0], data=data, x="NÚMERO LLIBRES", y="STATES", hue="TIPUS")
+    axes[0, 0].set_title('Number of Books vs States')
+    print('States plot saved')
+
+    sns.lineplot(ax=axes[0, 1], data=data, x="NÚMERO LLIBRES", y="MAXDEPTH", hue="TIPUS")
+    axes[0, 1].set_title('Number of Books vs Max Depth')
+    print('Max Depth plot saved')
+
+    sns.lineplot(ax=axes[1, 0], data=data, x="NÚMERO LLIBRES", y="EASY", hue="TIPUS")
+    axes[1, 0].set_title('Number of Books vs Easy Actions')
+    print('Easy Actions plot saved')
+
+    sns.lineplot(ax=axes[1, 1], data=data, x="NÚMERO LLIBRES", y="HARD ACTION", hue="TIPUS")
+    axes[1, 1].set_title('Number of Books vs Hard Actions')
+    print('Hard Actions plot saved')
+
+    # Save the plot as Experimento2.png
+    plt.savefig('./experiments/plots/experiment2.png')
+    plt.show()
+    print('experiment2.png saved.')
